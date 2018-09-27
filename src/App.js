b1c1c219430944a435dbf3fe030cb7dc0e97d27a
Brian Eisemann <eisemann@gmail.com>
2018-09-27 07:04:01 -0600

    title,favicon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 
 // import firebase from './firebase';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    document.title = "TaskPro"
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="App container">
+        <header>
+          <h1>TaskPro</h1>
+          <p>Search</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="row">
+
+          <div className="col-sm-4">
+            <div className="task-container">
+              <h2>Main</h2>
+              <p>list of all tasks</p>
+            </div>
+          </div>
+
+          <div className="col-sm-4">
+            <div className="task-container">
+              <h2>Priorities</h2>
+              <p>...</p>
+            </div>
+
+          </div>
+
+          <div className="col-sm-4">
+
+            <div className="task-container">
+              <h2>Deadlines</h2>
+              <p>...</p>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     );
   }
